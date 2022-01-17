fac3a5841c9695d9da3ea8d9236b71c2a466d4d9
Sam Westrick <shwestrick@gmail.com>
2022-01-17 16:43:18 -0500

    move sample server to sample-server, and dump other files

--- vscode-lsp/sample-server/src/server.ts
+++ vscode-lsp/sample-server/src/server.ts
@@ -70,6 +70,7 @@ connection.onInitialize((params: InitializeParams) => {
 
 connection.onInitialized(() => {
 	if (hasConfigurationCapability) {
+
 		// Register for all configuration changes.
 		connection.client.register(DidChangeConfigurationNotification.type, undefined);
 	}
